13f36c9dc703ed58cfb316cc9ade608a161b936d
ekball <90882418+ekball@users.noreply.github.com>
2021-11-06 14:12:33 -0400

    added preliminary buttons through js and preliminary save/load/clear functionality

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,12 @@
 // set empty arrays to store high scores and corresponding initials
-var score = [];
-var initials = [];
+var highScore = [
+    {
+        name: "",
+        score: 0
+    }
+];
+
+var currentScore = 0;
 
 // set initial time left at 200 sec until first question is given
 var timeLeft = 200;
@@ -9,57 +15,62 @@ var timeLeft = 200;
 var questionAndAnswers = [
 
     {
-        question = "Bootstrap was built using _______.",
-        answers = {
-            a = "Javascript",
-            b = "JQuery",
-            c = "CSS", 
-            d = "HTML"
+        question: "Bootstrap was built using _______.",
+        answers: {
+            a: "Javascript",
+            b: "JQuery",
+            c: "CSS", 
+            d: "HTML"
         },
-        solution = "c"
+        solution: "c"
     },
     {
-        question = "JQuery was built using _______.",
-        answers = {
-            a = "CSS",
-            b = "HTML",
-            c = "BootStrap", 
-            d = "Javascript"
+        question: "JQuery was built using _______.",
+        answers: {
+            a: "CSS",
+            b: "HTML",
+            c: "BootStrap", 
+            d: "Javascript"
         },
-        solution = "d"
+        solution: "d"
     },
     {
-        question = "HTML is used for creating a website's _______.",
-        answers = {
-            a = "structure",
-            b = "style",
-            c = "Javascript", 
-            d = "Web-API"
+        question: "HTML is used for creating a website's _______.",
+        answers: {
+            a: "structure",
+            b: "style",
+            c: "Javascript", 
+            d: "Web-API"
         },
-        solution = "a"
+        solution: "a"
     },
     {
-        question = "CSS is used for creating a website's _______.",
-        answers = {
-            a = "structure",
-            b = "Jquery",
-            c = "HTML", 
-            d = "style"
+        question: "CSS is used for creating a website's _______.",
+        answers: {
+            a: "structure",
+            b: "Jquery",
+            c: "HTML", 
+            d: "style"
         },
-        solution = "d"
+        solution: "d"
     },
     {
-        question = "Javascript is used for creating a website's _______.",
-        answers = {
-            a = "style",
-            b = "fine-tuend tasks",
-            c = "Bootstrap", 
-            d = "CSS"
+        question: "Javascript is used for creating a website's _______.",
+        answers: {
+            a: "style",
+            b: "fine-tuend tasks",
+            c: "Bootstrap", 
+            d: "CSS"
         },
-        solution = "b"
+        solution: "b"
     }
 
 ];
+
+
+// link id's from html to javascript
+var qAndASection = document.querySelector("#q-and-a-section");
+
 
 // main function
 var beginQuiz = function() {
@@ -154,12 +165,14 @@ var beginQuiz = function() {
     }
 };
 
-// save the user's initials  alongside the highscore
+// save the user's initials alongside the highscore
 var enterInitials = function () {
 
-    initials = window.prompt("Enter your initials: ");
+    highScore.name = window.prompt("Enter your initials: ");
 
-    return initials;
+    highScore.score = currentScore;
+
+    return highScore;
 
 };
 
@@ -170,12 +183,61 @@ var questionOne = function () {
 
 };
 
-// save the score to local storage
-var saveScore = function () {
+var createButtons = function () {
+
+    // create a header <h2> for the question
+    var questionEl = document.createElement("h2");
+    questionEl.className = "questions";
+    
+    // attach the header <h2> to the section of html for questions and answers
+    qAndASection.appendChild(buttonContainerEl);
+
+    // create ordered list for buttons
+    var buttonListEl = document.createElement("ol");
+    buttonListEl.className = "buttons";
+    
+    // attach the button list to the section of html for questions and answers
+    qAndASection.appendChild(buttonlistEl);
+
+    // create list items to hold buttons
+    var buttonListItemEl = document.createElement("li")
+
+    // create 4 buttons: A, B, C, and D, then attach the buttons to a button list items
+
+    var buttonA = document.createElement("button");
+    buttonListEl.appendChild(buttonListItemEl);
+
+    var buttonB = document.createElement("button");
+    buttonListEl.appendChild(buttonListItemEl);
+
+    var buttonC = document.createElement("button");
+    buttonListEl.appendChild(buttonListItemEl);
+
+    var buttonD = document.createElement("button");
+    buttonListEl.appendChild(buttonListItemEl);
 
 };
 
-// print the final score on the screen and ask for initials
-var finalScore = function () {
+// save the score to local storage
+var saveScore = function () {
+
+    localStorage.setItem('highScore', JSON.stringify(highScore));
+
+    console.log("score saved");
+};
+
+// load score from local storage
+var loadScore = function () {
+    
+    JSON.parse(localStorage.getItem('highscore'));
+
+    console.log("score loaded");
+
+};
+
+// clear local memory
+var clearScores = function () {
+
+    localStorage.clear();
 
 };
